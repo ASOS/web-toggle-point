e8c9594692a88eb6a8b152b8cc09ad29003fb67c
TomStrepsil <10725179+TomStrepsil@users.noreply.github.com>
2025-09-30 20:16:57 +0100

    clean up script

--- packages/prepare-publish.mjs
+++ packages/prepare-publish.mjs
@@ -2,21 +2,22 @@ import transformLinks from "transform-markdown-links";
 import { copyFile, readFile, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 
+const encoding = "utf8";
+
 (async function prePublish() {
   const packageDirectory = process.cwd();
-  const sourceReadme = join(packageDirectory, "./docs/README.md");
-  const targetReadme = join(packageDirectory, "README.md");
-  await copyFile(sourceReadme, targetReadme);
+
   const sourceLicense = join(packageDirectory, "../../LICENSE");
   const targetLicense = join(packageDirectory, "LICENSE");
   await copyFile(sourceLicense, targetLicense);
-  const readmeContent = await readFile(targetReadme, "utf8");
 
+  const sourceReadme = join(packageDirectory, "./docs/README.md");
+  const targetReadme = join(packageDirectory, "README.md");
+  const readmeContent = await readFile(sourceReadme, encoding);
   const transformedContent = transformLinks(readmeContent, (link) => {
     if (link.startsWith("../")) {
       return link.replace(/^\.\.\//, "");
     }
   });
-
-  await writeFile(targetReadme, transformedContent, "utf8");
+  await writeFile(targetReadme, transformedContent, encoding);
 })();
